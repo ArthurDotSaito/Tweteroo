dd45c026888d3c7cf5b68adccd27ffbe6325a3db
Arthur Saito <arthur_saito1998@hotmail.com>
2023-01-07 17:57:01 -0300

    Added POST

--- src/app.js
+++ src/app.js
@@ -8,6 +8,20 @@ const server = express();
 server.use(cors());
 server.use(json());
 
+server.post("/sign-up", (req, res) =>{
+    const userData = req.body
+
+    USERS.push(userData);
+    return res.sendStatus(200).send("OK!");
+})
+
+server.post("/tweets", (req, res) => {
+    const {username} = req.headers;
+    const {tweet} = req.body;
+
+    if(USERS.find((e) => e.username === username)) return res.sendStatus(401).send("UNAUTHORIZED");
+})
+
 server.listen(5000, () => {
     console.log(`Rodando em http://localhost:${PORT}`);
   })
