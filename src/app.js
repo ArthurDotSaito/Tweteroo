57a9262ffa397bb0b54a3d167683b8169dcfdda1
Arthur Saito <arthur_saito1998@hotmail.com>
2023-01-07 18:57:47 -0300

    GET TWEETS part-working

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 const TWEETS = [];
 const USERS = [];
 const PORT = 5000;
+const MAXTWEETS = 10;
 const server = express();
 server.use(cors());
 server.use(express.json());
@@ -13,12 +14,20 @@ server.post("/sign-up", (req, res) =>{
     if(!userData.username || !userData.avatar) return res.status(400).send("Todos os campos são obrigatórios!")
 
     USERS.push(userData);
-    return res.status(200).send("OK!");
+    return res.sendStatus(200);
+})
+
+server.get("/tweets", (req, res) => {
+    const page = req.query.page ? Number(req.query.page):1;
+    const tweets = [];
+    const max = TWEETS.length < MAXTWEETS * page ? TWEETS.length : MAXTWEETS * page;
+    const min = (MAXTWEETS * (page - 1)) + 0;
+    if(page < 1) return res.status(400).send("Informe uma página válida!");
+
+    return res.send(tweets);
 })
 
 server.post("/tweets", (req, res) => {
-    const {username} = req.headers;
-    const {tweet} = req.body;
 
     if(USERS.find((e) => e.username === username)) return res.sendStatus(401).send("UNAUTHORIZED");
 })
